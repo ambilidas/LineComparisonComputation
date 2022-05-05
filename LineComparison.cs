baafd930b167eb457898ace0697a849116dd5489
Ambili das v s <ambili.kan@gmail.com>
2022-05-05 18:07:39 +0530

    Calculated length of line

---
+++ LineComparison.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputation
+{
+    public class LineComparison
+    {
+       public void LengthOfLine()
+        {
+            Console.WriteLine("Enter the co-ordinates of first point");
+            Console.Write("x1= ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y1= ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the co-ordinates of second point");
+            Console.Write("x2= ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y2= ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Length = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
+            double LineLengh=Math.Sqrt(Length);
+            Console.WriteLine("Length of line : " + LineLengh);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace LineComparisonComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            LineComparison myobj=new LineComparison();
+            myobj.LengthOfLine();
         }
     }
 }
