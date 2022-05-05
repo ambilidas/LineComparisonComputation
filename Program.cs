ef6833999f8114baac94ab732f02092f2ea184fe
Ambili das v s <ambili.kan@gmail.com>
2022-05-05 17:32:29 +0530

    Line comparison computation program is added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+   public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
+
